d3c10fbee280d7c6472d8aa0fb64c7907a683ea9
NaagAlgates <nagharaj.cse@gmail.com>
2019-11-10 12:47:23 +1100

    Completed Saved screen

--- lib/ui/saved_deals.dart
+++ lib/ui/saved_deals.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:group_on_ui/helpers/themes.dart';
 
 class SavedDealsScreen extends StatefulWidget {
   @override
@@ -11,8 +14,41 @@ class SavedDealsScreenState extends State<SavedDealsScreen> {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      color: Colors.blue,
-        child: Text('Saved Deals', style: TextStyle(color: Colors.white, fontSize: 24.0)),
+      color: Colors.grey[300],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.favorite,
+            color: Colors.grey[400],
+            size: MediaQuery.of(context).size.width*0.3,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Text('You haven\'t added any deals',
+                      style: TextStyle(color: Colors.grey[700], fontSize: 22.0,fontWeight: FontWeight.bold)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width*0.6,
+            child: Text('Tap the hear icon on any deal to add it into your saved deals!',
+                style: TextStyle(color: Colors.grey[500], fontSize: 18.0,fontWeight: FontWeight.normal)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: groupOnThemeData.primaryColor,
+              onPressed: (){
+                log("Browse Clicked");
+              },
+              child: new Text("Browse"),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
